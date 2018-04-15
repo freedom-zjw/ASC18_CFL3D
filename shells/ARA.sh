49aab9e90f24e7a7fbd7a379f587fbfb4cc05abd
freedom-zjw <709075442@qq.com>
2018-04-15 21:01:12 +0800

    functions open for fhat.F

--- shells/ARA.sh
+++ shells/ARA.sh
@@ -1,8 +1,8 @@
 
 cd ../cfl3d.larc.nasa.gov/Cfl3dv6/3DTestcases/ARA_M100/ARA_M100
-splitter < ./split.inp_1blk 
-cfl3d_seq < ./cfl3d.inp_1blk 
-cat ./cfl3d.out | grep    'timing for complete run - time in seconds'   -A8
+#splitter < ./split.inp_1blk 
+#cfl3d_seq < ./cfl3d.inp_1blk 
+#cat ./cfl3d.out | grep    'timing for complete run - time in seconds'   -A8
 
 splitter < ./split.inp_16blk 
 mpirun -np 17 cfl3d_mpi < ./cfl3d.inp_16blk
--- shells/ARA_M100.sh
+++ shells/ARA_M100.sh
@@ -1,7 +1,8 @@
 cd ../cfl3d.larc.nasa.gov/Cfl3dv6/3DTestcases/ARA_M100/ARA_M100
 
 splitter < ./split.inp_16blk 
-amplxe-cl -collect hotspots mpirun -np 17 cfl3d_mpi < ./cfl3d.inp_16blk
+#amplxe-cl -collect hotspots mpirun -np 17 cfl3d_mpi < ./cfl3d.inp_16blk
+mpirun -np 17 cfl3d_mpi < ./cfl3d.inp_16blk
 cat ./cfl3d.out | grep    'timing for complete run - time in seconds'   -A24
 
 cd ../../../../../shells
