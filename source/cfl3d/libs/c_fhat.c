bae4bcf492fe768d64875dfec8288ebdea22c4a4
Enigma Huang <enigmahuang@gmail.com>
2018-04-21 18:00:38 -0400

    C rewritten fhat.F upper half
    Rewritten if (real(cprec) .eq. 0.) part.
    ARA_M100 test passed, result and performance is almost the same.

--- source/cfl3d/libs/c_fhat.c
+++ source/cfl3d/libs/c_fhat.c
@@ -2,18 +2,260 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
+#include <complex.h>
 
 // TODO: Move this macro to a head file and share among all C files
+// TODO: Use different math functions (abs, pow, ...) for different types
 #define FTYPE float
+#define SQRT  sqrtf
+#define FABS  fabsf
+#define POW   powf
+
+#define ind(i, j) ((i) + (j) * nvtq)
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 FTYPE *ax = NULL, *ay = NULL, *az = NULL, *area = NULL;
 FTYPE *at = NULL, *qr = NULL, *ql = NULL, *f    = NULL;
 
-void c_fhat_hookptr_(
+int n, iexp, nvtq;
+FTYPE gamma, gm1, gp1, gm1g, gp1g, ggm1, cprec;
+FTYPE uref, avn, epsa_l, epsa_r, x1, c1, zero;
+
+static void c_fhat_origin()
+{
+	// NOTICE: i is shifted to start from 0, we can directly use them;
+	// but for qr(), ql() and f(), the index of 2nd dimension need to -1.
+	for (int i = 0; i < n; i++)
+	{
+		// huangh223 added local variables: qr[1-5], ql[1-5], a[x-z,t]i
+		// These can avoid repeat index computation and make it convenient for coding.
+		// NOTICE: if the corresponding values in the array is modified,
+		// always keep these local variables up-to-date.
+		FTYPE qr1 = qr[ind(i, 0)];
+		FTYPE qr2 = qr[ind(i, 1)];
+		FTYPE qr3 = qr[ind(i, 2)];
+		FTYPE qr4 = qr[ind(i, 3)];
+		FTYPE qr5 = qr[ind(i, 4)];
+		
+		FTYPE ql1 = ql[ind(i, 0)];
+		FTYPE ql2 = ql[ind(i, 1)];
+		FTYPE ql3 = ql[ind(i, 2)];
+		FTYPE ql4 = ql[ind(i, 3)];
+		FTYPE ql5 = ql[ind(i, 4)];
+		
+		FTYPE axi = ax[i];
+		FTYPE ayi = ay[i];
+		FTYPE azi = az[i];
+		FTYPE ati = at[i];
+		
+		// ------ Original code translation start ------
+		// ----- Comments in original code is kept -----
+		
+		FTYPE t1 = qr1 - ql1;
+		FTYPE t2 = qr2 - ql2;
+		FTYPE t3 = qr3 - ql3;
+		FTYPE t4 = qr4 - ql4;
+		
+		// Pressure and enthalpy
+		FTYPE t16 = 1.0 / qr1;
+		FTYPE t5  = qr5;
+		qr5 = x1 * qr5 * t16 + 0.5 * (qr2 * qr2 + qr3 * qr3 + qr4 * qr4);
+		qr[ind(i, 4)] = qr5;  // Always keep qr* updated
+		
+		FTYPE t15 = 1.0 / ql1;
+		FTYPE t19 = ql5;
+		ql5 = x1 * ql5 * t15 + 0.5 * (ql2 * ql2 + ql3 * ql3 + ql4 * ql4);
+		ql[ind(i, 4)] = ql5;  // Always keep ql* updated
+		
+		// Unsplit contributions f(r) + f(l)
+		FTYPE t18 = axi * qr2 + ayi * qr3 + azi * qr4 + ati;
+		FTYPE t17 = axi * ql2 + ayi * ql3 + azi * ql4 + ati;
+		FTYPE t6  = t18 * qr1;
+		FTYPE t7  = t17 * ql1;
+		FTYPE f1  = t6 + t7;
+		FTYPE f2  = t6 * qr2 + t7 * ql2;
+		FTYPE f3  = t6 * qr3 + t7 * ql3;
+		FTYPE f4  = t6 * qr4 + t7 * ql4;
+		FTYPE f5  = t6 * qr5 + t7 * ql5;
+		FTYPE t8  = t5 + t19;
+		f2 += axi * t8;
+		f3 += ayi * t8;
+		f4 += azi * t8;
+		f5 -= ati * t8;
+		
+		// Roe averaged variables
+		t6 = qr1 * t15;
+		t7 = SQRT(t6);
+		t6 = 1.0 / (1.0 + t7);
+		t8 = t7 * t6;
+		
+		// Average density
+		qr1 = ql1 * t7;
+		qr[ind(i, 0)] = qr1;  // Always keep qr* updated
+		
+		// u, v, w, h average
+		FTYPE t9  = ql2 * t6 + qr2 * t8;
+		FTYPE t10 = ql3 * t6 + qr3 * t8;
+		FTYPE t11 = ql4 * t6 + qr4 * t8;
+		FTYPE t12 = ql5 * t6 + qr5 * t8;
+		
+		// Extract sound speed
+		t6  = (t9 * t9 + t10 * t10 + t11 * t11) * 0.5;
+		t7  = gm1 * (t12 - t6);
+		t8  = SQRT(t7);
+		FTYPE t13 = t9 * axi + t10 * ayi + t11 * azi;
+		
+		// The variables in t are as follows:
+		// 1-4        delta q1-q4
+		// 6          q2a
+		// 7          c2a
+		// 8          ca
+		// 9,10,11,12 ua, va, wa, ha
+		// 13         ubara
+		// 14,15,16   alpha(1),  alpha(4),  alpha(5)
+		// 18,19,17   lambda(1), lambda(4), lambda(5)
+		// rhoa*delta(ubar), delta(p)/c2a
+		
+		ql1 = qr1 * (t18 - t17);
+		ql2 = (t5 - t19) / t7;
+		ql[ind(i, 0)] = ql1;  // Always keep ql* updated
+		ql[ind(i, 1)] = ql2;  // Always keep ql* updated
+		
+		// Update t18 and t18 = abs(t18)
+		t18 = t13 + ati;
+		#ifdef C_CMPLX
+		if (creal(t18) < 0) t18 = -t18;
+		#else
+		t18 = FABS(t18);
+		#endif
+		
+		// Update t19 and t19 = abs(t19)
+		t19 = t13 + ati + t8;
+		#ifdef C_CMPLX
+		if (creal(t19) < 0) t19 = -t19;
+		#else
+		t19 = FABS(t19);
+		#endif
+		
+		// Update t17 and t17 = abs(t17)
+		t17 = t13 + ati - t8;
+		#ifdef C_CMPLX
+		if (creal(t17) < 0) t17 = -t17;
+		#else
+		t17 = FABS(t17);
+		#endif
+		
+		// Limit eigenvalues a la Harten and Gnoffo (NASA TP-2953)
+		if (creal(epsa_r) > 0) 
+		{
+			FTYPE cc = t8;
+			FTYPE uu = t9;
+			FTYPE vv = t10;
+			FTYPE ww = t11;
+			
+			#ifdef C_CMPLX
+			if (creal(uu) < 0) uu = -uu;
+			if (creal(vv) < 0) vv = -vv;
+			if (creal(ww) < 0) ww = -ww;
+			#else
+			uu = FABS(uu);
+			vv = FABS(vv);
+			ww = FABS(ww);
+			#endif
+			
+			FTYPE epsaa = epsa_r * (cc + uu + vv + ww);
+			
+			FTYPE temp;
+			#ifdef C_CMPLX
+			if (creal(epsaa) > creal(zero)) temp = epsaa; else temp = zero;
+			#else
+			temp = MAX(epsaa, zero);
+			#endif
+			
+			FTYPE epsbb = 0.25 / temp;
+			FTYPE epscc = 2.0  * epsaa;
+			
+			if (creal(t18) < creal(epscc)) t18 = t18 * t18 * epsbb + epsaa;
+			if (creal(t17) < creal(epscc)) t17 = t17 * t17 * epsbb + epsaa;
+			if (creal(t19) < creal(epscc)) t19 = t19 * t19 * epsbb + epsaa;
+		}
+		
+		FTYPE t14 = t18 * (t1 - ql2);
+		t15 = 0.5 * (ql2 + ql1 / t8);
+		t16 = (ql2 - t15) * t17;
+		t15 = t15 * t19;
+		
+		qr2 = t18 * (t2 * qr1 - axi * ql1);
+		qr3 = t18 * (t3 * qr1 - ayi * ql1);
+		qr4 = t18 * (t4 * qr1 - azi * ql1);
+		qr5 = t9 * qr2 + t10 * qr3 + t11 * qr4;
+		qr[ind(i, 1)] = qr2;  // Always keep qr* updated
+		qr[ind(i, 2)] = qr3;  // Always keep qr* updated
+		qr[ind(i, 3)] = qr4;  // Always keep qr* updated
+		qr[ind(i, 4)] = qr5;  // Always keep qr* updated
+		
+		ql1 = t14 + t15 + t16;
+		ql2 = t8 * (t15 - t16);
+		ql[ind(i, 0)] = ql1;  // Always keep ql* updated
+		ql[ind(i, 1)] = ql2;  // Always keep ql* updated
+		
+		f1 = f1 - ql1;
+		f2 = f2 - ql1 * t9  - axi * ql2 - qr2;
+		f3 = f3 - ql1 * t10 - ayi * ql2 - qr3;
+		f4 = f4 - ql1 * t11 - azi * ql2 - qr4;
+		f5 = f5 - ql1 * t12 - t13 * ql2 - qr5 + t7 * c1 * t14;
+		
+		// Introduce factor one-half area
+		t7 = 0.5 * area[i];
+		f[ind(i, 0)] = t7 * f1;
+		f[ind(i, 1)] = t7 * f2;
+		f[ind(i, 2)] = t7 * f3;
+		f[ind(i, 3)] = t7 * f4;
+		f[ind(i, 4)] = t7 * f5;
+	}
+}
+
+static void c_fhat_modified()
+{
+	for (int i = 0; i < n; i++)
+	{
+		
+	}
+}
+
+int iter = 0;
+
+void c_fhat_(
+	int *_n, int *_iexp, int *_nvtq, 
+	FTYPE *_gamma, FTYPE *_gm1, FTYPE *_gp1, FTYPE *_gm1g, FTYPE *_gp1g, 
+	FTYPE *_ggm1, FTYPE *_cprec, FTYPE *_uref, FTYPE *_avn, FTYPE *_epsa_r, 
 	FTYPE *_ax, FTYPE *_ay, FTYPE *_az, FTYPE *_area, 
 	FTYPE *_at, FTYPE *_qr, FTYPE *_ql, FTYPE *_f
 )
 {
+	// Single variables
+	n      = *_n;
+	iexp   = *_iexp;
+	nvtq   = *_nvtq;
+	gamma  = *_gamma;
+	gm1    = *_gm1;
+	gp1    = *_gp1;
+	gm1g   = *_gm1g;
+	gp1g   = *_gp1g;
+	ggm1   = *_ggm1;
+	cprec  = *_cprec;
+	uref   = *_uref;
+	avn    = *_avn;
+	epsa_r = *_epsa_r;
+	zero   = POW(10.0, (FTYPE) -iexp);
+	epsa_l = 2.0 * epsa_r;
+	x1     = gamma / gm1;
+	c1     = 1.0   / gm1;
+	
+	// Pointers
 	ax   = _ax;
 	ay   = _ay;
 	az   = _az;
@@ -22,28 +264,7 @@ void c_fhat_hookptr_(
 	qr   = _qr;
 	ql   = _ql;
 	f    = _f;
-}
-
-void c_fhat_(
-	int *_n, int *_iexp, FTYPE *_gamma, FTYPE *_gm1, FTYPE *_gp1, FTYPE *_gm1g, 
-	FTYPE *_gp1g, FTYPE *_ggm1, FTYPE *_cprec, FTYPE *_uref, FTYPE *_avn, 
-	FTYPE *_epsa_l, FTYPE *_epsa_r, FTYPE *_x1, FTYPE *_c1, FTYPE *_zero
-)
-{
-	int n    = *_n;
-	int iexp = *_iexp;
-	FTYPE gamma  = *_gamma;
-	FTYPE gm1    = *_gm1;
-	FTYPE gp1    = *_gp1;
-	FTYPE gm1g   = *_gm1g;
-	FTYPE gp1g   = *_gp1g;
-	FTYPE ggm1   = *_ggm1;
-	FTYPE cprec  = *_cprec;
-	FTYPE uref   = *_uref;
-	FTYPE avn    = *_avn;
-	FTYPE epsa_l = *_epsa_l;
-	FTYPE epsa_r = *_epsa_r;
-	FTYPE x1     = *_x1;
-	FTYPE c1     = *_c1;
-	FTYPE zero   = *_zero;
+	
+	if (FABS(cprec) < zero) c_fhat_origin();
+	//else c_fhat_modified();
 }
